e251912c0dc43cc3e76864ce350b185b95211612
이승언 <71816852+grukall@users.noreply.github.com>
2024-07-03 10:08:02 +0900

    feat : EDN-2220 material converter 수정

--- Assets/Editor/Resources/Screens/Export/Export.cs
+++ Assets/Editor/Resources/Screens/Export/Export.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,6 +15,9 @@ using UniVRM10;
 using VrmLib;
 using VRMShaders;
 using ScrollView = UnityEngine.UIElements.ScrollView;
+
+using lilToon;
+using Object = UnityEngine.Object;
 
 namespace Editor.Resources.Screens.Export
 {
@@ -77,6 +81,98 @@ namespace Editor.Resources.Screens.Export
             LoadItems();
         }
         
+        private static void ChangeMaterials(GameObject prefab, string savePath)
+        {
+            //1. prefab 하위 오브젝트들을 활성화시킨다.
+            List<GameObject> list = new List<GameObject>();
+            Stack<Transform> stack = new Stack<Transform>();
+            stack.Push(prefab.transform);
+            
+            while (stack.Count > 0)
+            {
+                Transform current = stack.Pop();
+
+                foreach (Transform child in current)
+                {
+                    if (!child.gameObject.activeSelf)
+                    {
+                        child.gameObject.SetActive(true);
+                        list.Add(child.gameObject);
+                    }
+
+                    stack.Push(child);
+                }
+            }
+            var setActiveFalseList = list.ToArray();
+            
+            
+            //2.prefab의 skinmeshrenderer을 모아 이중 for each문으로 각각의 material을 가져온다.
+            var skinnedMeshRenderers = prefab.GetComponentsInChildren<SkinnedMeshRenderer>();
+            List<Material> referenceMaterials = new List<Material>();
+
+            foreach (var skinnedMesh in skinnedMeshRenderers)
+            {
+                foreach (var sMaterial in skinnedMesh.sharedMaterials)
+                {
+                    //3. lilToon인지 확인하고, 맞다면 referenceMaterial 라스트애 추가한다.
+                    if (sMaterial != null && sMaterial.shader.name.Contains("lilToon"))
+                    {
+                        if (!referenceMaterials.Contains(sMaterial))
+                        {
+                            referenceMaterials.Add(sMaterial);
+                        }
+                    }
+                }
+            }
+            
+            
+            //4.referneceMaterials를 순환하면 Mtoon으로 재질 변환한다. 변환한 material들은 Dictionary 형식으로 저장한다.
+            Dictionary<Material, Material> convertedMaterials = new Dictionary<Material, Material>();
+            foreach (var material in referenceMaterials)
+            {
+                var path = Path.Combine(savePath,
+                    material.name + ".mat");
+                try
+                {
+                    lilMaterialBaker.CreateMToonMaterial(material, path);
+                }
+                catch (Exception e)
+                {
+                    //Debug.Log(material.name + "Exception : " + e);
+                }
+
+                Material m = AssetDatabase.LoadAssetAtPath(path, typeof(Material)) as Material;
+                if (!m)
+                {
+                    Debug.Log("no Materials : " + path);
+                }
+                convertedMaterials.Add(material, m);
+            }
+            
+            
+            //다시 skinMesh단위로 순환하며 Dictionary의 meterial들을 map에서 찾아 교체한다.
+            foreach (var skinnedMesh in skinnedMeshRenderers)
+            {
+                var materials = skinnedMesh.sharedMaterials;
+                for (int i = 0; i < materials.Length; i++)
+                {
+                    if (materials[i] != null && materials[i].shader.name.Contains("lilToon"))
+                    {
+                        if (convertedMaterials.ContainsKey(materials[i]))
+                        {
+                            materials[i] = convertedMaterials[materials[i]];
+                        }
+                    }
+                }
+
+                skinnedMesh.sharedMaterials = materials;
+            }
+            
+            //오브잭트들을 다시 비활성화한다.
+            setActiveFalseList.ToList().ForEach(obj => obj.SetActive(false));
+            
+        }
+        
         private static void ExportItem()
         {
             // Export the selected item
@@ -95,50 +191,32 @@ namespace Editor.Resources.Screens.Export
 
             // 이 프리팹을 사용
             prefab = prefabClone;
+            
+            //prefab의 material들의 셰이더를 Mtoon으로 변환
+            ChangeMaterials(prefab, "Assets/Eden/");
 
             using (var arrayManager = new NativeArrayManager())
             {
                 var converter = new ModelExporter();
                 var model = converter.Export(arrayManager, prefab);
-
+               
                 model.ConvertCoordinate(Coordinates.Vrm1);
+                
+                //셰이더를 Mtoon에서 Mtoon10으로 변환 및 _ShadTexture 속성을 임시 저장 및 _ShadeTex 속성으로 복구
                 foreach (var VARIABLE  in model.Materials)
                 {
                     var material = VARIABLE as Material;
-                    Debug.Log(material);
                     if (material == null) continue;
-                    Debug.Log(material.shader);
+
+                    Texture ShadeTexture = material.GetTexture("_ShadeTexture");
+                    
+                    // Change the shader
                     material.shader = Shader.Find("VRM10/MToon10");
 
-                    // set shade texture to same as main texture
                     if (material.HasProperty("_ShadeTex"))
                     {
-                        material.SetTexture("_ShadeTex", material.GetTexture("_MainTex"));
+                        material.SetTexture("_ShadeTex", ShadeTexture);
                     }
-
-                    // _BumpMap 은 _MainTex 가 null 이 아닐 때만 적용, material name + "_normal" 을 찾아서 적용
-                    if (material.HasProperty("_BumpMap") && material.GetTexture("_MainTex") != null)
-                    {
-                        Debug.Log(material.name);
-                        // 해당 이름을 가진 texture guid, texture2D, 확장자 png
-                        var guids = AssetDatabase.FindAssets(material.name + "_normal t:texture2D glob:\"*.png\"");
-
-                        if (guids.Length != 0)
-                        {
-                            var textureGuid = guids.FirstOrDefault();
-                            // 해당 guid 를 가진 texture path
-                            var texturePath = AssetDatabase.GUIDToAssetPath(textureGuid);
-                            // 해당 texture path 에 있는 texture 를 가져옴
-                            Debug.Log(texturePath);
-                            var normalTexture = AssetDatabase.LoadAssetAtPath<Texture2D>(texturePath);
-
-                            if (normalTexture != null)
-                            {
-                                material.SetTexture("_BumpMap", normalTexture);
-                            }
-                        }
-                    }
-                    Debug.Log(material.shader);
                 }
 
                 var gltfExportSettings = new GltfExportSettings
@@ -190,7 +268,7 @@ namespace Editor.Resources.Screens.Export
                 await Task.Yield();
             }
         }
-
+        
         public void CreateGUI()
         {
             // Each editor window contains a root VisualElement object
