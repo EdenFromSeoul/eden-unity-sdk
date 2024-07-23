71668de3e88c4e95b1e3756b05c955ca21c33808
Ilan Kim <deusta12@eden-world.net>
2024-07-23 15:09:25 +0900

    Add MPL-2.0 License info

--- Assets/Editor/Scripts/Manager/ConvertManager.cs
+++ Assets/Editor/Scripts/Manager/ConvertManager.cs
@@ -1,3 +1,22 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Original Code: https://github.com/esperecyan/VRMConverterForVRChat
+ * Initial Developer: esperecyan
+ *
+ * Alternatively, the contents of this file may be used under the terms
+ * of the MIT license (the "MIT License"), in which case the provisions
+ * of the MIT License are applicable instead of those above.
+ * If you wish to allow use of your version of this file only under the
+ * terms of the MIT License and not to allow others to use your version
+ * of this file under the MPL, indicate your decision by deleting the
+ * provisions above and replace them with the notice and other provisions
+ * required by the MIT License. If you do not delete the provisions above,
+ * a recipient may use your version of this file under either the MPL or
+ * the MIT License.
+ */
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -93,7 +112,9 @@ namespace Editor.Scripts.Manager
             string path,
             GameObject gameObject,
             VRMMetaObject vrmMetaObject,
-            IDictionary<string, List<BlendShapeData>> selectedBlendShapes)
+            IDictionary<string, List<BlendShapeData>> selectedBlendShapes,
+            bool removeUnusedBlendShapes = true
+            )
         {
             try
             {
@@ -196,12 +217,14 @@ namespace Editor.Scripts.Manager
                 // );
 
                 // 모든 skinned mesh renderer의 shared mesh에서 clean up
-                foreach (var renderer in gameObject.GetComponentsInChildren<SkinnedMeshRenderer>())
+                if (removeUnusedBlendShapes)
                 {
-                    SkinnedMesh.CleanUpShapeKeysVrm0(renderer.sharedMesh, necessaryShapeKeys);
+                    foreach (var renderer in gameObject.GetComponentsInChildren<SkinnedMeshRenderer>())
+                    {
+                        SkinnedMesh.CleanUpShapeKeysVrm0(renderer.sharedMesh, necessaryShapeKeys);
+                    }
+                    // SkinnedMesh.CleanUpShapeKeysVrm0(combinedRenderer.sharedMesh, necessaryShapeKeys);
                 }
-
-                // SkinnedMesh.CleanUpShapeKeysVrm0(combinedRenderer.sharedMesh, necessaryShapeKeys);
 
                 TabMeshSeparator.SeparationProcessing(gameObject);
 
