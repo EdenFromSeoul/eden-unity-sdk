aef3a6708b7116b4ab8e4a831ee7d8c447003558
Ilan Kim <deusta12@eden-world.net>
2024-07-31 13:45:23 +0900

    feat(language): add language manager

--- Assets/Editor/Resources/EdenStudio.cs
+++ Assets/Editor/Resources/EdenStudio.cs
@@ -1,5 +1,6 @@
 using System;
 using Editor.Resources.Screens.Export;
+using Editor.Scripts.Manager;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -14,6 +15,9 @@ namespace Editor.Resources
 
         public static StyleSheet style;
         public static StyleSheet tailwind;
+        private static Label prefab_list_label;
+        private static Label settings_label;
+        private static EnumField languageDropdown;
 
         [MenuItem("Window/EdenStudio")]
         public static void ShowExample()
@@ -32,12 +36,33 @@ namespace Editor.Resources
             root.styleSheets.Add(style);
             root.styleSheets.Add(tailwind);
 
-
             // Instantiate UXML
             VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
             root.Add(labelFromUXML);
             container = labelFromUXML.Q("container");
+            prefab_list_label = labelFromUXML.Q<Label>("prefab_list_label");
+            settings_label = labelFromUXML.Q<Label>("settings_label");
+            languageDropdown = labelFromUXML.Q<EnumField>("language_field");
+            
+            LocalizationManager.OnLanguageChanged += () =>
+            {
+                prefab_list_label.text = LocalizationManager.GetLocalizedValue("prefab_list");
+                settings_label.text = LocalizationManager.GetLocalizedValue("settings");
+                languageDropdown.label = LocalizationManager.GetLocalizedValue("language");
+            };
+            
+            languageDropdown.RegisterValueChangedCallback(OnLanguageChanged);
+            prefab_list_label.text = LocalizationManager.GetLocalizedValue("prefab_list");
+            settings_label.text = LocalizationManager.GetLocalizedValue("settings");
+            languageDropdown.label = LocalizationManager.GetLocalizedValue("language");
             Export.Show(container, OnExportVrmClicked);
+        }
+
+        private void OnLanguageChanged(ChangeEvent<Enum> evt)
+        {
+            Debug.Log($"Language changed to {evt.newValue}");
+            var language = (Language)evt.newValue;
+            LocalizationManager.SetLanguage(language);
         }
 
         private void OnExportVrmClicked()
